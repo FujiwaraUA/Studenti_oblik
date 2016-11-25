7825daccb3f681cff0d1562415b0918beb4350bf
SerkoUA <serko_x@ukr.net>
2016-11-25 13:06:46 +0200

    Django 1.7 Динамізуємо список студентів

--- students/views.py
+++ students/views.py
@@ -5,7 +5,24 @@ from django.http import HttpResponse
 # Views for Students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': 'Віталій',
+         'last_name': 'Кононенко',
+         'ticket': 235,
+         'image': 'img/me1.jpg'},
+        {'id': 2,
+         'first_name': 'Андрій',
+         'last_name': 'Іваненко',
+         'ticket': 2123,
+         'image': 'img/me.jpeg'},
+        {'id': 3,
+         'first_name': 'Василь',
+         'last_name': 'Шевчено',
+         'ticket': 21,
+         'image': 'img/piv.png'},
+    )
+    return render(request, 'students/students_list.html', {'students': students})
 
 
 def students_add(request):
